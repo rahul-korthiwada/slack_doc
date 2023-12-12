b03a3f7a0ae799ec0cde42e6f8029536761f971f
eswar2001 <eswar.tadiparth@juspay.in>
2023-12-12 16:49:43 +0530

    added logic for google api integration

---
+++ src/docs.py
@@ -0,0 +1,43 @@
+# reference docs : https://developers.google.com/resources/api-libraries/documentation/docs/v1/python/latest/docs_v1.documents.html
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/documents"]
+
+def create_google_document(title='My Document',body={
+            'title': 'My Document'
+        }):
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("docs", "v1", credentials=creds)
+        doc = service.documents() \
+            .create(body=body).execute()
+        print('Created document with title: {0}'.format(
+        doc.get('title')))
+
+    except HttpError as err:
+        print(err)
