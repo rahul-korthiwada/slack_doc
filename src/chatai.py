ccb5cd94be7d6b4f46de68846eda94df60f7cb3f
Rahul Korthiwada <rahul.korthiwada@juspay.in>
2024-01-30 10:17:32 +0530

    Adds Child Thread Context to Request

--- src/chatai.py
+++ src/chatai.py
@@ -5,7 +5,7 @@ import os
 def post_request_to_chat_gpt(parsedData):
     api_key = os.getenv("OPEN_API_KEY")
     api_endpoint = 'https://api.openai.com/v1/chat/completions'
-    model_name = 'gpt-3.5-turbo'
+    model_name = 'gpt-4-1106-preview'
 
 
     headers = {
@@ -14,16 +14,25 @@ def post_request_to_chat_gpt(parsedData):
     }
 
     data = {
-        'model': 'gpt-3.5-turbo',
+        'model': 'gpt-4-1106-preview',
         'messages': [
             {'role': 'user', 'content': json.dumps(parsedData)},
-            {'role': 'user', 'content': 'Can you provide a detailed information for the above Slack Conversation, Json Format :-  Array Of Messages, Each Message contains message, the user who posted it and the peoples reactions. Please provide the summary in the following format 1. Summary of the problem 2. Why it has happened 3. Different Kind Of Solutions 4. Actionables 5.Actual Session Information'},
+            {'role': 'user', 'content': '''Can you provide a detailed summary for the above slack conversation. Pick the context from the appropriate messages and share summary in the following format:
+                                            1. Summary of the problem being discussed on the thread
+                                            2. What caused the issue/problem?
+                                            3. Who got affected and what was the impact?
+                                            4. Actual session information where the problem happened?
+                                            5. What were the all the checks discussed(or suggested) on the thread to understand the problem better and root cause the issue?
+                                            6. What are the final action steps taken to resolve the issue?
+                                            7. What are the steps taken to resolve this issue with specific info on each step.
+                                            8. What are the steps taken for better visibility and debugging.'''},
             # {'role':'user','content':'Hello'}
         ],
     }
-
+    print("request_sent")
+    print(json.dumps(data))
     response = requests.post(api_endpoint, headers=headers, json=data)
-
+    print(response.json())
     if response.status_code == 200:
         result = response.json()
         title_data = {
